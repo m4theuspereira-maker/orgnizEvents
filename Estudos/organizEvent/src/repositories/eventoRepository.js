7a2ee0ab4f0cf5fa5b522afd879cba8633401bb7
m4theuspereira-maker <matheusmonaco1@hotmail.com>
2021-04-07 17:02:48 -0300

    add callback

--- Estudos/organizEvent/src/repositories/eventoRepository.js
+++ Estudos/organizEvent/src/repositories/eventoRepository.js
@@ -1,94 +1,98 @@
 
-const {getUsuarioAtual} = require('../repositories/userRepository')
-const {d, firebase} = require('../app')
+const { getUsuarioAtual } = require('../repositories/userRepository')
+const { d, firebase } = require('../app')
 
 const createEvento = async (evento) => {
-  const result =  await  db.collection('eventos').add(evento).then(() => {
-      console.log('evento salvo?')
-    }).catch(() => {
-      console.log('evento não salvo')
+  const result = await db.collection('eventos').add(evento).then(() => {
+    console.log('evento salvo?')
+  }).catch(() => {
+    console.log('evento não salvo')
+  })
+
+  return result
+}
+
+
+const getEventos = async () => {
+  let result = []
+  await db.collection('eventos').get().then(snapshot => {
+    return snapshot.docs.forEach(evento => {
+      result.push(evento.data())
     })
+  })
+  return result
+}
 
-    return result
-  }
+const getEventosByUsuarioId = async () => {
+  try {
 
-
-  const getEventos = async () => {
+    const usuarioAtual = await getUsuarioAtual()
     let result = []
-    await db.collection('eventos').get().then(snapshot => {
-      return snapshot.docs.forEach(evento => {        
-          result.push(evento.data())
+    await firebase.firestore().collection('eventos').where("usuarioId", "==", `${usuarioAtual.uid}`).get().then(snapshot => {
+      return snapshot.docs.forEach((evento) => {
+        result.push(evento.data())
       })
     })
+
     return result
+
+  } catch (error) {
+    console.log(error)
   }
 
-  const getEventosByUsuarioId = async () =>{
-    try{
-
-      const usuarioAtual = await getUsuarioAtual()
-
-      const result = await firebase.firestore().collection('eventos').where("usuarioId", "==", `${usuarioAtual.uid}`).get()
-
-      return result
-
-    }catch(error){
-      console.log(error)
-    }
-    
-  }
+}
 
 
-  const atualizarEvento = async (id, descricao) => {
-    return await db.collection('eventos').doc(id).update({ descricao: descricao }).then(() => {
-      console.log('Atualizado com sucesso')
-    }).catch(() => {
-      console.log('erro ao atualizar')
+const atualizarEvento = async (id, descricao) => {
+  return await db.collection('eventos').doc(id).update({ descricao: descricao }).then(() => {
+    console.log('Atualizado com sucesso')
+  }).catch(() => {
+    console.log('erro ao atualizar')
+  })
+}
+
+//atualizarEvento('7Yip40CmtxgVHvs3BKKr', 'qualquer coisa')
+
+const deletar = async (id) => {
+  return await db.collection('evento').doc(id).delete().then(() => {
+    console.log('deletado')
+  }).catch(() => {
+    console.log('deu erro')
+  })
+}
+
+
+
+const findSubDocument = async (documentId, nomeCollection) => {
+  return await db.collection('eventos').doc(documentId).collection(nomeCollection).get().then((snapshot) => {
+    snapshot.forEach(doc => {
+      console.log(doc.data())
     })
-  }
-  
-  //atualizarEvento('7Yip40CmtxgVHvs3BKKr', 'qualquer coisa')
-  
-  const deletar = async (id) => {
-    return await db.collection('evento').doc(id).delete().then(() => {
-      console.log('deletado')
-    }).catch(() => {
-      console.log('deu erro')
-    })
-  }
+  }).catch(() => {
+    console.log('error')
+  })
+}
 
-  
-
-  const findSubDocument = async (documentId, nomeCollection) => {
-   return await db.collection('eventos').doc(documentId).collection(nomeCollection).get().then((snapshot) => {
-      snapshot.forEach(doc => {
-        console.log(doc.data())
-      })
-    }).catch(() => {
-      console.log('error')
-    })
-  }
-  
-  const findById = async (id) => {
-   return await db.collection('eventos').doc(id).get()
+const findById = async (id) => {
+  return await db.collection('eventos').doc(id).get()
     .then((document) => {
       console.log(document.data())
       return document.data()
     }).catch(() => {
       console.log('deu erro')
     })
-   
-  }
+
+}
 
 
 
-module.exports ={
+module.exports = {
   getEventosByUsuarioId,
-  createEvento, 
-  getEventos, 
-  atualizarEvento, 
-  deletar, 
-  findSubDocument, 
+  createEvento,
+  getEventos,
+  atualizarEvento,
+  deletar,
+  findSubDocument,
   findById
 }
 
